017f239d6a9a601d9785d8dfea441bbb490a7fa9
luca <gm.lollators@gmail.com>
2018-10-21 20:55:15 -0400

    Corrected misspell of towels, added Window name

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Housekeeping.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Housekeeping Request");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }

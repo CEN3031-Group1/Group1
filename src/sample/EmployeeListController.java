79da44001230bb60f70a7f14f15761abce7655fb
Natt913 <42674567+Natt913@users.noreply.github.com>
2018-11-07 13:11:51 -0500

    11/7/2018

---
+++ src/sample/EmployeeListController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeListController {
+
+    @FXML
+    TableView employeeTable;
+    @FXML
+    TableColumn employeeName;
+    @FXML
+    TableColumn employeePosition;
+    @FXML
+    TableColumn employeePresent;
+
+
+}
